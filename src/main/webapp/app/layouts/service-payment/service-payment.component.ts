1833b259b157ec09aa31dfa304f656b906900887
Marvin Calvo <mar90jesus@gmail.com>
2020-11-06 20:56:34 -0600

    Se modifico detalle del pago

--- src/main/webapp/app/layouts/service-payment/service-payment.component.ts
+++ src/main/webapp/app/layouts/service-payment/service-payment.component.ts
@@ -1,19 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IPayPalConfig, ICreateOrderRequest } from 'ngx-paypal';
+import { IPublishingPackage } from 'app/shared/model/publishing-package.model';
+import { Subscription } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { PublishingPackageService } from '../../entities/publishing-package/publishing-package.service';
+import { Account } from 'app/core/user/account.model';
+import { JhiEventManager } from 'ng-jhipster';
 @Component({
   selector: 'jhi-service-payment',
   templateUrl: './service-payment.component.html',
   styleUrls: ['./service-payment.component.scss'],
 })
-export class ServicePaymentComponent implements OnInit {
+export class ServicePaymentComponent implements OnInit, OnDestroy {
   public payPalConfig?: IPayPalConfig;
   public showSuccess = true;
   public order: any;
-  constructor() {}
+  public account: Account | null = null;
+  public eventSubscriber?: Subscription;
+  public publishingPackages: IPublishingPackage[] | [] = [];
+  public publishingPackageSelected?: IPublishingPackage;
+  constructor(protected publishingPackageService: PublishingPackageService, protected eventManager: JhiEventManager) {
+    this.publishingPackageSelected = {} as IPublishingPackage;
+  }
   ngOnInit(): void {
+    this.loadAll();
+    this.registerChangeInPublishingPackages();
+  }
+  ngOnDestroy(): void {
+    if (this.eventSubscriber) {
+      this.eventManager.destroy(this.eventSubscriber);
+    }
+  }
+  loadAll(): void {
+    this.publishingPackageService.query().subscribe((res: HttpResponse<IPublishingPackage[]>) => {
+      this.publishingPackages = res.body || [];
+      for (let i = 0; 4 < this.publishingPackages.length || i < this.publishingPackages.length; i++) {
+        this.publishingPackages.pop();
+      }
+    });
+  }
+  registerChangeInPublishingPackages(): void {
+    this.eventSubscriber = this.eventManager.subscribe('publishingPackageListModification', () => this.loadAll());
+  }
+  trackId(index: number, item: IPublishingPackage): number {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    return item.id!;
+  }
+  setServiceIndex(index: number): void {
+    this.publishingPackageSelected = this.publishingPackages[index];
     this.initConfig();
   }
-  public getPackage() {}
   public initConfig(): void {
     this.payPalConfig = {
       clientId: 'AUhE1PVR_p5RwD-Rdvd9uD0zV_cSrJ6c933Hlmr3Dnls7KhpbS0Cfsn8uunzo_UIySZ5nv_HBCgbJqW2',
@@ -25,23 +61,22 @@ export class ServicePaymentComponent implements OnInit {
               description: 'Paquete publicitario para subastas y promoción de alquiler',
               amount: {
                 currency_code: 'USD',
-                value: '1',
+                value: '4.00',
                 breakdown: {
                   item_total: {
                     currency_code: 'USD',
-                    value: '1',
+                    value: '4.00',
                   },
                 },
               },
               items: [
                 {
-                  sku: 'Hola',
-                  name: 'Subscription a paquete de publicidad',
+                  name: 'Con este paquete publicitario puede publicar propiedades durante ' + this.publishingPackageSelected?.cantDays,
                   quantity: '1',
                   category: 'DIGITAL_GOODS',
                   unit_amount: {
                     currency_code: 'USD',
-                    value: '1',
+                    value: '4.00',
                   },
                 },
               ],
--- src/main/webapp/app/layouts/service-payment/service-payment.module.ts
+++ src/main/webapp/app/layouts/service-payment/service-payment.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxPayPalModule } from 'ngx-paypal';
 import { ServicePaymentComponent } from 'app/layouts/service-payment/service-payment.component';
+import { servicePaymentRoute } from './service-payment.route';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ServicePaymentComponent],
   exports: [ServicePaymentComponent],
-  imports: [CommonModule, NgxPayPalModule],
+  imports: [CommonModule, NgxPayPalModule, RouterModule.forRoot([servicePaymentRoute], { useHash: true }), FormsModule],
 })
 export class ServicePaymentModule {}
